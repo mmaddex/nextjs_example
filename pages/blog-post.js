370d88632c4ac03fc911cc9233282263c16287f8
matt maddex <mmaddex@render.com>
2023-06-13 17:43:58 -0600

    Update blog-post.js

--- pages/blog-post.js
+++ pages/blog-post.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   console.log('getting static props');
   // {
   //   "data": {
@@ -20,31 +20,51 @@ export async function getStaticProps() {
   //     }
   //   }
   // }
-  const res = await fetch('https://api.github.com/graphql', {
-    method: 'POST',
-    headers: {
-      Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-    },
-    body: JSON.stringify({
-      query: `query {
-        repository(owner:"vercel", name:"reactions") {
-          issue(number:1) {
-            reactionGroups {
-              content
-              users(first: 0) {
-                totalCount
+  if (context.draftMode) {
+    const json = {
+       "data": {
+         "repository": {
+           "issue": {
+             "reactionGroups": [
+               { "content": "THUMBS_UP", "users": { "totalCount": 0 } },
+               { "content": "THUMBS_DOWN", "users": { "totalCount": 0 } },
+               { "content": "LAUGH", "users": { "totalCount": 0 } },
+               { "content": "HOORAY", "users": { "totalCount": 0 } },
+               { "content": "CONFUSED", "users": { "totalCount": 0 } },
+               { "content": "HEART", "users": { "totalCount": 0 } },
+               { "content": "ROCKET", "users": { "totalCount": 0 } },
+               { "content": "EYES", "users": { "totalCount": 0 } }
+             ]
+           }
+         }
+       }
+     }
+  } else {
+    const res = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      headers: {
+        Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+      },
+      body: JSON.stringify({
+        query: `query {
+          repository(owner:"vercel", name:"reactions") {
+            issue(number:1) {
+              reactionGroups {
+                content
+                users(first: 0) {
+                  totalCount
+                }
               }
             }
           }
-        }
-      }`,
-    }),
-  });
-
-  const json = await res.json();
-  if (res.status !== 200) {
-    console.error(json);
-    throw new Error('Failed to fetch API');
+        }`,
+      }),
+    });
+    const json = await res.json();
+    if (res.status !== 200) {
+      console.error(json);
+      throw new Error('Failed to fetch API');
+    }
   }
 
   // [0, 0, 0, 0, 0, 0, 0, 0]
