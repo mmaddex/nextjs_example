634abbf644a60de9b33a1971ee2334a54aae1b4e
matt maddex <mmaddex@render.com>
2023-06-13 17:09:19 -0600

    Update draft.js

--- pages/api/draft.js
+++ pages/api/draft.js
@@ -1,6 +1,12 @@
 import { draftMode } from 'next/headers'
+import { redirect } from 'next/navigation'
  
 export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const slug = searchParams.get('slug')
+  // In reality, this would call the headless CMS
+  const post = await getPostBySlug(slug)
   draftMode().enable()
+  redirect(post.slug)
   return new Response('Draft mode is enabled')
 }
