ae830d9c0cc318445a00b136576dbc264fa1430d
matt maddex <mmaddex@render.com>
2023-06-14 15:37:35 -0600

    Create renderedago.js

---
+++ pages/renderedago.js
@@ -0,0 +1,20 @@
+'use client'
+ 
+import { useState } from 'react'
+ 
+export default function RenderedAgo(renderedAt) {
+  const [renderedAgo, setRenderedAgo] = useState(0)
+  /*const updateTime = () => {
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+    const clockElement = document.getElementById('clock');
+    
+  }*/
+  setInterval(setRenderedAgo(new Date() - new Date(renderedAt)), 1000)
+ 
+  return (
+    <div>
+      <p id="clock">Rendered {renderedAgo} ago</p>
+    </div>
+  )
+}
