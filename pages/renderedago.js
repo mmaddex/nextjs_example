a3f99f95bf53292ca10e531171cac15aa1ab8fc6
matt maddex <mmaddex@render.com>
2023-06-14 16:28:59 -0600

    Update renderedago.js

--- pages/renderedago.js
+++ pages/renderedago.js
@@ -5,11 +5,10 @@ import { useState } from 'react'
 export default function RenderedAgo({ renderedAt }) {
   const [renderedAgo, setRenderedAgo] = useState(0)
   setInterval((() => setRenderedAgo(parseInt((new Date() - new Date(renderedAt))/1000))), 1000)
-  const fixedra = parseInt((new Date() - new Date(renderedAt))/1000)
+  setRenderedAgo(parseInt((new Date() - new Date(renderedAt))/1000))
   return (
     <div>
       <p>Rendered {renderedAgo} seconds ago</p>
-      <p>Rendered {fixedra} seconds before the page was loaded</p>
     </div>
   )
 }
