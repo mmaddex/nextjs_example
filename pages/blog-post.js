a10647e2eaf904b8900324601dc882249645c757
matt maddex <mmaddex@render.com>
2023-06-14 14:32:41 -0600

    Update blog-post.js

--- pages/blog-post.js
+++ pages/blog-post.js
@@ -218,7 +218,7 @@ export default function Home({ reactions, draftTitle, renderedAt }) {
               const clockElement = document.getElementById('clock');
               clockElement.textContent = 'Current time: ' + time;
             }}
-            setInterval({function(){updateTime()}}, 1000)
+            const testing = setInterval(updateTime, 1000)
           </Script>
       </main>
     </div>
