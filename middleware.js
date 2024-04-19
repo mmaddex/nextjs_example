8f0b6842f86bf95073314c2a594af6a221231c8e
mmaddex <mmaddex@render.com>
2024-04-19 13:57:50 -0700

    fix thi string

--- middleware.js
+++ middleware.js
@@ -12,5 +12,5 @@ export function middleware(request) {
   //const response = NextResponse.next();
   //response.headers.set('Cache-Control', 'public');
   //return response;
-  return NextResponse.rewrite(new URL('/api/edge_runtime', process.env.RENDER_EXTERNAL_HOSTNAME))
+  return NextResponse.rewrite(new URL('/api/edge_runtime', `http://${process.env.RENDER_EXTERNAL_HOSTNAME}`))
 }
