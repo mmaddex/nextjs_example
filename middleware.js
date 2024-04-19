9202e4a2e73b9cf1e4b448af571b94ea93548401
mmaddex <mmaddex@render.com>
2024-04-19 13:45:55 -0700

    edge only functionality

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
  
+export const config = {
+  matcher: '/api_it',
+};
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
   console.log('in the middleware');
@@ -8,4 +12,5 @@ export function middleware(request) {
   //const response = NextResponse.next();
   //response.headers.set('Cache-Control', 'public');
   //return response;
+  return NextResponse.rewrite('/api/edge_runtime');
 }
